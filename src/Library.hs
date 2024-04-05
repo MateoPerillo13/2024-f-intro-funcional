43bd1517fe49ca9062dd517c2b934303cc6d5155
Mateo Perillo <mperillo@frba.utn.edu.ar>
2024-04-05 17:35:18 -0300

    Funciones desarrolladas

--- src/Library.hs
+++ src/Library.hs
@@ -1,14 +1,29 @@
 module Library where
 import PdePreludat
 
-suma :: Number -> Number -> Number
-suma x y = x + y
-
-doble :: Number -> Number
-doble x = x + x
-
 totalEmpleados :: Number -> String -> Number
 totalEmpleados cantidadSucursales nombreEmpresa = cantidadEmpleadosPorSucursal nombreEmpresa * cantidadSucursales
+
+terminaConLetraMenor :: String -> Bool
+terminaConLetraMenor palabra = head palabra > last palabra
+
+letrasIntermedias :: String -> Number
+letrasIntermedias palabra = (length palabra) - 2
+
+esCapicua :: String -> Bool
+esCapicua palabra
+    | (head palabra == last palabra) && (length palabra > 2) = esCapicua(tail (init palabra))
+    | (head palabra == last palabra) = True
+    | otherwise = False
+
+
+nombreEsPar :: String -> Bool
+nombreEsPar nombre = rem (length nombre) 2 == 0
+
+esMultiploDe :: String -> Number -> Bool 
+esMultiploDe palabra multiplo = rem (length palabra) multiplo == 0
+
+
 
 cantidadEmpleadosPorSucursal :: String -> Number
 cantidadEmpleadosPorSucursal "acme" = 10 --Pattern matching
@@ -16,24 +31,7 @@ cantidadEmpleadosPorSucursal "acme" = 10 --Pattern matching
 cantidadEmpleadosPorSucursal nombreEmpresa --Guardas
     | terminaConLetraMenor nombreEmpresa = letrasIntermedias nombreEmpresa
     | esCapicua nombreEmpresa && nombreEsPar nombreEmpresa = letrasIntermedias nombreEmpresa * 2
+    | esMultiploDe nombreEmpresa 3 || esMultiploDe nombreEmpresa 7 = 3
     | otherwise = 0
 
 --Tarea: Implementar funciones, y la condicion que falta (esMultiploDe)
-
-
-
-
-
-
-
-        
-
-
-
-modulo :: Number -> Number
-modulo x 
-    | x >= 0 = x
-    | x < 0 = negate x
-
-
-
